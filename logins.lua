6d108ef8790fea6218d56da9033588e759dd8f18
Marcos <m19494123@gmail.com>
2025-11-29 21:19:10 -0300

    cadastro de logins

---
+++ logins.lua
@@ -0,0 +1,14 @@
+Pessoa = {}
+Pessoa._index = Pessoa
+function Pessoa:new(cor, nome)
+    obj = setmetatable({},Pessoa)
+    obj.nome = nome
+    obj.cor = cor
+    return obj
+end
+function Pessoa:status()
+    print("seu nome e: "..self.nome)
+    print("sua cor e: "..self.cor)
+end
+p1 = Pessoa:new("marcos","branca")
+Pessoa:status()
